f7285d6db91db8a8c2fa4e5feba4f216bea6b45a
PT-70 <paulyjd@gmail.com>
2020-06-08 09:42:38 +0000

    issue with git not commiting updated

--- run.py
+++ run.py
@@ -9,19 +9,19 @@ def index():
 
 @app.route('/recipes')
 def recipes():
-    return render_template('recipes.html')
+    return render_template('recipes.html', page_title='Simple recipes to make you look like a pro!')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', page_title='Contact us')
 
 @app.route('/grills')
 def contact():
-    return render_template('grills.html')
+    return render_template('grills.html', page_title='What grill should I get?')
 
 @app.route('/tips')
 def contact():
-    return render_template('tips.html')
+    return render_template('tips.html', page_title='FAQ')
 
     if __name__ == '__main__':
         app.run(host=os.environ.get('IP'),
