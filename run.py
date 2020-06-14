afa06e651de7dd4dc5e9b06f04548c898bb6eaa1
PT-70 <paulyjd@gmail.com>
2020-06-14 14:27:31 +0000

    tidy up pages

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import os
 import json
 from flask import Flask, render_template, redirect, request, flash, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
-  import env 
+    import env
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -22,9 +22,11 @@ mongo = PyMongo(app)
 def index():
     return render_template('index.html')
 
+
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 @app.route('/grills')
 def grills():
@@ -54,6 +56,7 @@ def recipes_recipes(recipes_name):
 
     return '<h1>' + recipes['ingredients'] + '</h1>'
 
+
 @app.route('/tips')
 def tips():
     data = []
@@ -61,45 +64,49 @@ def tips():
         data = json.load(json_data)
     return render_template('tips.html', page_title="FAQ'S", tips=data)
 
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        flash('Thanks {}, We have received your message'.format(request.form['name']))
+        flash('Thanks {}, We have received your message'.format(
+            request.form['name']))
     return render_template('contact.html', page_title='Contact')
+
 
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html')
-                         
 
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks =  mongo.db.tasks
+    tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
 
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     return render_template('edittask.html', task=the_task)
 
 
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
-    tasks.update( {'_id': ObjectId(task_id)},  
-  {
-        'task_name':request.form.get('task_name'),
+    tasks.update({'_id': ObjectId(task_id)},
+                 {
+        'task_name': request.form.get('task_name'),
         'task_description': request.form.get('task_description'),
     })
     return redirect(url_for('get_tasks'))
+
 
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
